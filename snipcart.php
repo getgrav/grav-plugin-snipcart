1a5c115fd6ef68e28b4bfbeadb4554347359382c
Andy Miller <rhuk@mac.com>
2014-08-19 19:05:13 -0600

    Refactoring per the new page events system

--- snipcart.php
+++ snipcart.php
@@ -12,16 +12,16 @@ class SnipcartPlugin extends Plugin
      */
     public static function getSubscribedEvents() {
         return [
-            'onAfterGetPage' => ['onAfterGetPage', 0],
-            'onAfterTwigTemplatesPaths' => ['onAfterTwigTemplatesPaths', 0],
-            'onAfterTwigSiteVars' => ['onAfterTwigSiteVars', 0]
+            'onPageInitialized' => ['onPageInitialized', 0],
+            'onTwigTemplatePaths' => ['onTwigTemplatePaths', 0],
+            'onTwigSiteVariables' => ['onTwigSiteVariables', 0]
         ];
     }
 
     /**
      * Initialize configuration
      */
-    public function onAfterGetPage()
+    public function onPageInitialized()
     {
         $defaults = (array) $this->config->get('plugins.snipcart');
 
@@ -37,7 +37,7 @@ class SnipcartPlugin extends Plugin
     /**
      * Add current directory to twig lookup paths.
      */
-    public function onAfterTwigTemplatesPaths()
+    public function onTwigTemplatePaths()
     {
         $this->grav['twig']->twig_paths[] = __DIR__ . '/templates';
     }
@@ -45,7 +45,7 @@ class SnipcartPlugin extends Plugin
     /**
      * Set needed variables to display cart.
      */
-    public function onAfterTwigSiteVars()
+    public function onTwigSiteVariables()
     {
         if ($this->config->get('plugins.snipcart.built_in_css')) {
 
